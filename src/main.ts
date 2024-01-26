9faff4eef4e3402dfef2d71a5128222160435f46
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2024-01-26 13:14:49 +0000

    removed unused deps

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { AppModule } from './app.module'
 import { swaggerConfig, swaggerOptions } from './config/swagger'
 import { validationPipe } from './config/validation'
 import { ClassSerializerInterceptor } from '@nestjs/common'
-import { apiReference } from '@scalar/nestjs-api-reference'
 // import { QueryExceptionFilter } from './filters/exception.filter'
 
 async function bootstrap() {
@@ -15,9 +14,6 @@ async function bootstrap() {
   // app.useGlobalFilters(new QueryExceptionFilter())
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('api', app, document, swaggerOptions)
-
-  app.use('/reference', apiReference({ spec: { content: document } }))
-
   app.useGlobalPipes(validationPipe)
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
   await app.listen(3000)
